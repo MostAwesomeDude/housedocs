164cd68c3728042b6e4c2cd2772257fd9abc6728
Corbin <cds@corbinsimpson.com>
2023-10-17 16:31:42 -0700

    Reimplement contextual explanations.
    I'm not sure why this is slow and bad, but I can guess! It seems like
    MosaicML's weights are not especially good when compared to Meta's LLaMa
    weights. Additionally, it seems like there's some sort of contention
    issue with generation; eight threads are spawned over four cores, which
    might be too many threads.

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import numpy as np
 from faiss import IndexFlatIP, normalize_L2
 from sentence_transformers import SentenceTransformer
 
+from llm_rs import AutoModel, KnownModels
+from llm_rs.config import GenerationConfig
+
 from fastapi import FastAPI
 
+print("Loading sentence transformer...")
 embedder = SentenceTransformer("all-miniLM-L6-v2")
 EMBEDDING_WIDTH = 384
 index = IndexFlatIP(EMBEDDING_WIDTH)
@@ -19,9 +23,10 @@ def normedEmbeds(arr):
     normalize_L2(rv)
     return rv
 
+print("Loading document...")
 with open("docs.md") as handle:
     corpus = handle.read()
-indices = findIndices(corpus, ("\n", ".", "?", "!"))
+indices = findIndices(corpus, ("\n\n", ".", "?", "!"))
 spans = staggerIndices(indices) + staggerIndices(indices[1:])
 sentences = [corpus[i:j] for (i, j) in spans]
 index.add(normedEmbeds(embedder.encode(sentences)))
@@ -42,6 +47,11 @@ def searchFor(sentence):
     D, I = index.search(xq, 5)
     return mergeSpans([spans[i] for i in I[0]])
 
+print("Loading generative model...")
+model = AutoModel.from_pretrained("rustformers/mpt-7b-ggml",
+                                  model_file="mpt-7b-instruct-q4_0-ggjt.bin")
+
+print("Loading HTTP API...")
 app = FastAPI()
 
 @app.get("/")
@@ -51,3 +61,22 @@ def root(): return {}
 def search(sentence):
     ss = mergeSpans(searchFor(sentence))
     return [corpus[i:j].strip() for (i, j) in ss]
+
+PROMPT_TEMPLATE = """Here is an example query and response. The response includes details from the excerpts.
+Query: {q}
+Excerpts: {spans}
+Response:"""
+
+CONFIG = GenerationConfig(
+    max_new_tokens=200,
+    stop_words=["\n"],
+)
+
+@app.get("/explain")
+def explain(sentence):
+    ss = mergeSpans(searchFor(sentence))
+    spans = "\n * ".join([corpus[i:j].strip() for (i, j) in ss])
+    prompt = PROMPT_TEMPLATE.format(q=sentence, spans=spans)
+    return {
+        "text": model.generate(prompt, generation_config=CONFIG).text,
+    }
